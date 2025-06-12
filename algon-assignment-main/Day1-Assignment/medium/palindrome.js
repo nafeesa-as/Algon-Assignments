c9dc5cc440fbf79caaae51c0a141704015c6f59c
Nafeesa A S <129583587+nafeesa-as@users.noreply.github.com>
2025-06-12 17:48:01 +0530

    Update palindrome.js

--- algon-assignment-main/Day1-Assignment/medium/palindrome.js
+++ algon-assignment-main/Day1-Assignment/medium/palindrome.js
@@ -3,23 +3,13 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 function isPalindrome(str) {
-  
-  if (!str || typeof str !== 'string') {
-    return false;
-  }
-  const cleanStr = str.toLowerCase();
-  let left = 0;
-  let right = cleanStr.length - 1;
-  
-  while (left < right) {
-    if (cleanStr[left] !== cleanStr[right]) {
-      return false;
-    }
-    left++;
-    right--;
-  }
-  
-  return true;
+  if (typeof str !== 'string') return false;
+
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleaned === cleaned.split('').reverse().join('');
 }
+
+module.exports = isPalindrome;
+
 
 module.exports = isPalindrome;
